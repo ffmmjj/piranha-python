d512f00f4f7ff4195a814005bf81a940486bef8d
Felipe Martins <ffmmjj@gmail.com>
2021-06-02 13:55:54 -0300

    Add basic setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='piranha-python',
+    version='0.01',
+    packages=[''],
+    url='https://github.com/ffmmjj/piranha-python',
+    license='MIT',
+    author='felipe',
+    author_email='ffmmjj@gmail.com',
+    description=''
+)
