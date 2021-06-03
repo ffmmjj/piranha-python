945ba9e801ea349066ed9b04feaeefe7cb9fcdb7
Felipe Martins <ffmmjj@gmail.com>
2021-06-03 16:26:05 -0300

    Remove code outside feature flag block if it contains an unconditional return statement

--- src/piranha.py
+++ src/piranha.py
@@ -1,4 +1,4 @@
-from libcst import FlattenSentinel, matchers
+from libcst import FlattenSentinel, RemoveFromParent, matchers
 from libcst.codemod import VisitorBasedCodemodCommand
 
 
@@ -19,7 +19,23 @@ class PiranhaCommand(VisitorBasedCodemodCommand):
     def __init__(self, context, flag_name):
         super().__init__(context)
         self.flag_name = flag_name
+        self.is_in_feature_flag_block = False
+        self.found_return_stmt_in_ff_block = False
+
+    def visit_If(self, node):
+        self.is_in_feature_flag_block = matchers.matches(node.test, matchers.Name(self.flag_name))
+        return True
 
     def leave_If(self, original_node, updated_node):
-        if matchers.matches(original_node.test, matchers.Name(self.flag_name)):
+        if self.is_in_feature_flag_block:
+            return_statements = matchers.findall(original_node.body, matchers.Return())
+            self.found_return_stmt_in_ff_block = len(return_statements) > 0
+
+            self.is_in_feature_flag_block = False
             return FlattenSentinel(original_node.body.body)
+
+    def leave_SimpleStatementLine(self, original_node, updated_node):
+        if not self.is_in_feature_flag_block and self.found_return_stmt_in_ff_block:
+            return RemoveFromParent()
+
+        return updated_node
--- test/test_transformer.py
+++ test/test_transformer.py
@@ -6,29 +6,46 @@ from piranha import PiranhaCommand
 
 class PiranhaCodemodTest(CodemodTest):
     TRANSFORM = PiranhaCommand
+    FEATURE_FLAG_NAME = "FEATURE_FLAG_NAME"
 
     def test_keeps_IF_block_body_having_single_non_return_expression(self):
-        feature_flag_name = "FEATURE_FLAG_NAME"
-        original_code = _as_clean_str(
-            """\
-        if %s:
-            print('Flag is active')
-
-        print('This is not related to the feature flag value at all')
-        """
-            % feature_flag_name
-        )
-
         self.assertCodemod(
-            original_code,
             _as_clean_str(
                 """\
-                        print('Flag is active')
+            if %s:
+                print('Flag is active')
 
-                        print('This is not related to the feature flag value at all')
-                        """
+            print('This is not related to the feature flag value at all')
+            """
+                % self.FEATURE_FLAG_NAME
             ),
-            flag_name=feature_flag_name,
+            _as_clean_str(
+                """\
+            print('Flag is active')
+
+            print('This is not related to the feature flag value at all')
+            """
+            ),
+            flag_name=self.FEATURE_FLAG_NAME,
+        )
+
+    def test_keeps_only_IF_block_body_when_it_has_an_unconditional_return_expression(self):
+        self.assertCodemod(
+            _as_clean_str(
+                """\
+            if %s:
+                return 0
+
+            print('This is not related to the feature flag value at all')
+            """
+                % self.FEATURE_FLAG_NAME
+            ),
+            _as_clean_str(
+                """\
+            return 0
+            """
+            ),
+            flag_name=self.FEATURE_FLAG_NAME,
         )
 
 
