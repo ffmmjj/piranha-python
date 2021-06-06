63d9f487f2981d75eb2d5c7f273cda95796a7d6f
Felipe Martins <ffmmjj@gmail.com>
2021-06-05 21:53:12 -0300

    Group similar tests in separate test classes for better navigability

--- test/test_codemods.py
+++ test/test_codemods.py
@@ -3,10 +3,11 @@ import textwrap
 from libcst.codemod import CodemodContext, CodemodTest
 from piranha.codemods import PiranhaCommand
 
+FEATURE_FLAG_NAME = "FEATURE_FLAG_NAME"
+
 
 class PiranhaCodemodTest(CodemodTest):
     TRANSFORM = PiranhaCommand
-    FEATURE_FLAG_NAME = "FEATURE_FLAG_NAME"
 
     def test_keeps_IF_block_body_having_single_non_return_expression(self):
         self.assertCodemod(
@@ -17,7 +18,7 @@ class PiranhaCodemodTest(CodemodTest):
 
             print('This is not related to the feature flag value at all')
             """
-                % self.FEATURE_FLAG_NAME
+                % FEATURE_FLAG_NAME
             ),
             _as_clean_str(
                 """\
@@ -26,7 +27,7 @@ class PiranhaCodemodTest(CodemodTest):
             print('This is not related to the feature flag value at all')
             """
             ),
-            flag_name=self.FEATURE_FLAG_NAME,
+            flag_name=FEATURE_FLAG_NAME,
         )
 
     def test_keeps_only_IF_block_body_when_it_has_an_unconditional_return_expression(self):
@@ -38,14 +39,14 @@ class PiranhaCodemodTest(CodemodTest):
 
             print('This is not related to the feature flag value at all')
             """
-                % self.FEATURE_FLAG_NAME
+                % FEATURE_FLAG_NAME
             ),
             _as_clean_str(
                 """\
             return 0
             """
             ),
-            flag_name=self.FEATURE_FLAG_NAME,
+            flag_name=FEATURE_FLAG_NAME,
         )
 
     def test_ignores_IF_block_when_processing_a_test_module(self):
@@ -56,12 +57,12 @@ class PiranhaCodemodTest(CodemodTest):
 
             print('This is not related to the feature flag value at all')
             """
-            % self.FEATURE_FLAG_NAME
+            % FEATURE_FLAG_NAME
         )
         self.assertCodemod(
             if_block_without_return_stmt,
             if_block_without_return_stmt,
-            flag_name=self.FEATURE_FLAG_NAME,
+            flag_name=FEATURE_FLAG_NAME,
             context_override=_test_module_context(),
         )
 
@@ -72,12 +73,12 @@ class PiranhaCodemodTest(CodemodTest):
 
             print('This is not related to the feature flag value at all')
             """
-            % self.FEATURE_FLAG_NAME
+            % FEATURE_FLAG_NAME
         )
         self.assertCodemod(
             if_block_with_return_stmt,
             if_block_with_return_stmt,
-            flag_name=self.FEATURE_FLAG_NAME,
+            flag_name=FEATURE_FLAG_NAME,
             context_override=_test_module_context(),
         )
 
@@ -89,113 +90,13 @@ class PiranhaCodemodTest(CodemodTest):
 
             print('This is not related to the feature flag value at all')
             """
-            % self.FEATURE_FLAG_NAME
+            % FEATURE_FLAG_NAME
         )
         self.assertCodemod(
             if_block_without_return_stmt,
             if_block_without_return_stmt,
-            flag_name=self.FEATURE_FLAG_NAME,
+            flag_name=FEATURE_FLAG_NAME,
             ignored_module_check_fn_path="test.test_codemods._always_return_true",
-        )
-
-    def test_keeps_single_lined_docstring_in_module(self):
-        self.assertCodemod(
-            _as_clean_str(
-                """\
-            \"\"\" Single lined docstring for this module \"\"\"
-            if %s:
-                return 0
-
-            print('This is not related to the feature flag value at all')
-            """
-                % self.FEATURE_FLAG_NAME
-            ),
-            _as_clean_str(
-                """\
-            \"\"\" Single lined docstring for this module \"\"\"
-            return 0
-            """
-            ),
-            flag_name=self.FEATURE_FLAG_NAME,
-        )
-
-    def test_keeps_multi_lined_docstring_in_module(self):
-        self.assertCodemod(
-            _as_clean_str(
-                """\
-            \"\"\"
-            Multi lined
-            docstring for this module
-            \"\"\"
-            if %s:
-                return 0
-
-            print('This is not related to the feature flag value at all')
-            """
-                % self.FEATURE_FLAG_NAME
-            ),
-            _as_clean_str(
-                """\
-            \"\"\"
-            Multi lined
-            docstring for this module
-            \"\"\"
-            return 0
-            """
-            ),
-            flag_name=self.FEATURE_FLAG_NAME,
-        )
-
-    def test_keeps_single_lined_docstring_in_function(self):
-        self.assertCodemod(
-            _as_clean_str(
-                """\
-            def func1():
-                \"\"\" Single lined docstring for this function \"\"\"
-                if %s:
-                    return 0
-
-                print('This is not related to the feature flag value at all')
-            """
-                % self.FEATURE_FLAG_NAME
-            ),
-            _as_clean_str(
-                """\
-            def func1():
-                \"\"\" Single lined docstring for this function \"\"\"
-                return 0
-            """
-            ),
-            flag_name=self.FEATURE_FLAG_NAME,
-        )
-
-    def test_keeps_multi_lined_docstring_in_function(self):
-        self.assertCodemod(
-            _as_clean_str(
-                """\
-            def func1():
-                \"\"\"
-                Multi lined
-                docstring for this module
-                \"\"\"
-                if %s:
-                    return 0
-
-                print('This is not related to the feature flag value at all')
-            """
-                % self.FEATURE_FLAG_NAME
-            ),
-            _as_clean_str(
-                """\
-            def func1():
-                \"\"\"
-                Multi lined
-                docstring for this module
-                \"\"\"
-                return 0
-            """
-            ),
-            flag_name=self.FEATURE_FLAG_NAME,
         )
 
     def test_can_work_with_multiple_defs_in_module(self):
@@ -216,7 +117,7 @@ class PiranhaCodemodTest(CodemodTest):
                     if %(flag_name)s:
                         print('Flag is active')
                 """
-                % {"flag_name": self.FEATURE_FLAG_NAME}
+                % {"flag_name": FEATURE_FLAG_NAME}
             ),
             _as_clean_str(
                 """\
@@ -229,10 +130,118 @@ class PiranhaCodemodTest(CodemodTest):
                     print('This is not related to the feature flag value at all')
                     print('Flag is active')
                 """
-                % {"flag_name": self.FEATURE_FLAG_NAME}
+                % {"flag_name": FEATURE_FLAG_NAME}
             ),
-            flag_name=self.FEATURE_FLAG_NAME,
+            flag_name=FEATURE_FLAG_NAME,
         )
+
+
+class PiranhaCodemodDocstringsTest(CodemodTest):
+    TRANSFORM = PiranhaCommand
+
+    def test_keeps_single_lined_docstring_in_module(self):
+        self.assertCodemod(
+            _as_clean_str(
+                """\
+            \"\"\" Single lined docstring for this module \"\"\"
+            if %s:
+                return 0
+
+            print('This is not related to the feature flag value at all')
+            """
+                % FEATURE_FLAG_NAME
+            ),
+            _as_clean_str(
+                """\
+            \"\"\" Single lined docstring for this module \"\"\"
+            return 0
+            """
+            ),
+            flag_name=FEATURE_FLAG_NAME,
+        )
+
+    def test_keeps_multi_lined_docstring_in_module(self):
+        self.assertCodemod(
+            _as_clean_str(
+                """\
+            \"\"\"
+            Multi lined
+            docstring for this module
+            \"\"\"
+            if %s:
+                return 0
+
+            print('This is not related to the feature flag value at all')
+            """
+                % FEATURE_FLAG_NAME
+            ),
+            _as_clean_str(
+                """\
+            \"\"\"
+            Multi lined
+            docstring for this module
+            \"\"\"
+            return 0
+            """
+            ),
+            flag_name=FEATURE_FLAG_NAME,
+        )
+
+    def test_keeps_single_lined_docstring_in_function(self):
+        self.assertCodemod(
+            _as_clean_str(
+                """\
+            def func1():
+                \"\"\" Single lined docstring for this function \"\"\"
+                if %s:
+                    return 0
+
+                print('This is not related to the feature flag value at all')
+            """
+                % FEATURE_FLAG_NAME
+            ),
+            _as_clean_str(
+                """\
+            def func1():
+                \"\"\" Single lined docstring for this function \"\"\"
+                return 0
+            """
+            ),
+            flag_name=FEATURE_FLAG_NAME,
+        )
+
+    def test_keeps_multi_lined_docstring_in_function(self):
+        self.assertCodemod(
+            _as_clean_str(
+                """\
+            def func1():
+                \"\"\"
+                Multi lined
+                docstring for this module
+                \"\"\"
+                if %s:
+                    return 0
+
+                print('This is not related to the feature flag value at all')
+            """
+                % FEATURE_FLAG_NAME
+            ),
+            _as_clean_str(
+                """\
+            def func1():
+                \"\"\"
+                Multi lined
+                docstring for this module
+                \"\"\"
+                return 0
+            """
+            ),
+            flag_name=FEATURE_FLAG_NAME,
+        )
+
+
+class PiranhaCodemodFlagDeclarationRemovalTest(CodemodTest):
+    TRANSFORM = PiranhaCommand
 
     def test_removes_simple_flag_declaration_statement(self):
         self.assertCodemod(
@@ -245,7 +254,7 @@ class PiranhaCodemodTest(CodemodTest):
 
             print('This is not related to the feature flag value at all')
             """
-                % {"flag_name": self.FEATURE_FLAG_NAME}
+                % {"flag_name": FEATURE_FLAG_NAME}
             ),
             _as_clean_str(
                 """\
@@ -254,7 +263,7 @@ class PiranhaCodemodTest(CodemodTest):
             print('This is not related to the feature flag value at all')
             """
             ),
-            flag_name=self.FEATURE_FLAG_NAME,
+            flag_name=FEATURE_FLAG_NAME,
         )
 
     def test_removes_only_flag_declaration_from_multiple_assignment_of_same_value_statement(self):
@@ -268,7 +277,7 @@ class PiranhaCodemodTest(CodemodTest):
 
             print('This is not related to the feature flag value at all')
             """
-                % {"flag_name": self.FEATURE_FLAG_NAME}
+                % {"flag_name": FEATURE_FLAG_NAME}
             ),
             _as_clean_str(
                 """\
@@ -278,7 +287,7 @@ class PiranhaCodemodTest(CodemodTest):
             print('This is not related to the feature flag value at all')
             """
             ),
-            flag_name=self.FEATURE_FLAG_NAME,
+            flag_name=FEATURE_FLAG_NAME,
         )
 
     def test_removes_only_flag_declaration_from_multiple_assignment_via_tuple_unpacking(self):
@@ -292,7 +301,7 @@ class PiranhaCodemodTest(CodemodTest):
 
             print('This is not related to the feature flag value at all')
             """
-                % {"flag_name": self.FEATURE_FLAG_NAME}
+                % {"flag_name": FEATURE_FLAG_NAME}
             ),
             _as_clean_str(
                 """\
@@ -302,7 +311,7 @@ class PiranhaCodemodTest(CodemodTest):
             print('This is not related to the feature flag value at all')
             """
             ),
-            flag_name=self.FEATURE_FLAG_NAME,
+            flag_name=FEATURE_FLAG_NAME,
         )
 
 
