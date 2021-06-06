a48f88637e74fc1c9b630e305551b3aef7f013c2
Felipe Martins <ffmmjj@gmail.com>
2021-06-06 12:14:30 -0300

    Fix dependency version format in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version="0.0.1.dev2",
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
-    install_requires=["libcst>=0,<1"],
+    install_requires=["libcst<1"],
     url="https://github.com/ffmmjj/piranha-python",
     license="MIT",
     author="Felipe Martins",
