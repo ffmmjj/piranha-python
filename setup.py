40ea8688b3fbee3539845cfd7b92cb7b17c3c67b
Felipe Martins <ffmmjj@gmail.com>
2021-06-07 20:20:14 -0300

    Bump dev version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="piranha-python",
-    version="0.0.1.dev3",
+    version="0.0.1.dev4",
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     install_requires=["libcst<1"],
